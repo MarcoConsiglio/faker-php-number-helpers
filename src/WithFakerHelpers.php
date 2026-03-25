62fce458bd6d7ad3de685ecb460b0feb2dd57c36
Marco Consiglio <mrccnsgl@gmail.com>
2026-03-25 11:26:16 +0000

    feat: BREAKING replace non-static trait with static trait

--- src/WithFakerHelpers.php
+++ src/WithFakerHelpers.php
@@ -3,15 +3,26 @@ namespace MarcoConsiglio\FakerPhpNumberHelpers;
 
 use Faker\Factory;
 use Faker\Generator;
-use MarcoConsiglio\FakerPhpNumberHelpers\Random\Integer\Positive;
-use Nsfisis\NextAfter\NextAfter;
-use RoundingMode;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\Negative as NegativeFloat;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\NegativeExceptZero;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\NegativeFraction;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\Positive as PositiveFloat;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\PositiveExceptZero;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\PositiveFraction;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\Relative as RelativeFloat;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\RelativeExceptZero;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Float\RelativeFraction;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Integer\Negative as NegativeInteger;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Integer\Positive as PositiveInteger;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Integer\PositiveExceptZero as PositiveIntegerExceptZero;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Integer\Relative as RelativeInteger;
+use MarcoConsiglio\FakerPhpNumberHelpers\Random\Integer\RelativeExceptZero as RelativeIntegerExceptZero;
 
 /**
  * FakerPHP support trait that provides helper functions to easily generate 
- * random numbers.
+ * random numbers inside PHPUnit data providers.
  */
-trait WithFakerHelpers
+trait WithStaticFakerHelpers
 {
     /**
      * The maximum `float` value that can still have a fractional part. This 
@@ -24,23 +35,23 @@ trait WithFakerHelpers
     /**
      * The FakerPHP random generator.
      */
-    protected Generator $faker;
+    protected static Generator $faker;
 
     /**
      * Set up the FakerPHP generator.
      * 
      * @codeCoverageIgnore
      */
-    protected function setUpFaker(): void
+    protected static function setUpFaker(): void
     {
-        if (! isset($this->faker))
-            $this->faker = Factory::create(Factory::DEFAULT_LOCALE);
+        if (! isset(self::$faker))
+            self::$faker = Factory::create(Factory::DEFAULT_LOCALE);
     }
 
     /**
      * Return true if `$value` is zero.
      */
-    private function isZero(int|float $value): bool
+    private static function isZero(int|float $value): bool
     {
         return $value == 0;
     }
@@ -48,7 +59,7 @@ trait WithFakerHelpers
     /**
      * Return true if `$value` is positive, false otherwise.
      */
-    private function isPositive(int|float $value): bool
+    private static function isPositive(int|float $value): bool
     {
         return $value >= 0;
     }
@@ -56,7 +67,7 @@ trait WithFakerHelpers
     /**
      * Return true if `$value` is negative, false otherwise.
      */
-    private function isNegative(int|float $value): bool
+    private static function isNegative(int|float $value): bool
     {
         return $value < 0;
     }
@@ -65,231 +76,164 @@ trait WithFakerHelpers
      * Return true if both `$value_1` and `$value_2` are negative, false 
      * otherwise.
      */
-    private function areBothNegative(int|float $value_1, int|float $value_2): bool
+    private static function areBothNegative(int|float $value_1, int|float $value_2): bool
     {
-        return $this->isNegative($value_1) && $this->isNegative($value_2);
+        return self::isNegative($value_1) && self::isNegative($value_2);
     }
 
     /**
      * Return true if both `$value_1` and `$value_2` are positive, false 
      * otherwise.
      */
-    private function areBothPositive(int|float $value_1, int|float $value_2): bool
+    private static function areBothPositive(int|float $value_1, int|float $value_2): bool
     {
-        return $this->isPositive($value_1) && $this->isPositive($value_2);
+        return self::isPositive($value_1) && self::isPositive($value_2);
     }
 
     /**
      * Return a random relative integer.
      */
-    protected function randomInteger(int $min = PHP_INT_MIN, int $max = PHP_INT_MAX): int
+    protected static function randomInteger(int $min = PHP_INT_MIN, int $max = PHP_INT_MAX): int
     {
-        if ($this->areBothNegative($min, $max))
-            return $this->negativeRandomInteger($min, $max);
-        if ($this->areBothPositive($min, $max))
-            return $this->positiveRandomInteger($min, $max);
-        if ($this->faker->boolean)
-            return $this->positiveRandomInteger(0, $max);
-        else
-            return $this->negativeRandomInteger($min, -1);
+        return new RelativeInteger(self::$faker, new IntRange($min, $max))->generate();
     }
 
     /**
      * Return a positive random integer.
      */
-    protected function positiveRandomInteger(int $min = 0, int $max = PHP_INT_MAX): int
+    protected static function positiveRandomInteger(int $min = 0, int $max = PHP_INT_MAX): int
     {
-        return new Positive(self::$faker, new IntRange($min, $max))->generate();
+        return new PositiveInteger(self::$faker, new IntRange($min, $max))->generate();
     }
 
     /**
      * Return a negative random integer.
      */
-    protected function negativeRandomInteger(int $min = PHP_INT_MIN + 1, int $max = -1): int
+    protected static function negativeRandomInteger(int $min = PHP_INT_MIN + 1, int $max = -1): int
     {
-        $min = $this->limitNegativeInteger($min);
-        $max = $this->limitNegativeInteger($max);
-        if ($this->isPositive($min)) $min = -2;
-        if ($this->isPositive($max)) $max = -1;
-        return -$this->faker->numberBetween(abs($max), abs($min));
+        return new NegativeInteger(self::$faker, new IntRange($min, $max))->generate();
     }
 
     /**
      * Return a positive random integer except for zero.
      */
-    protected function positiveNonZeroRandomInteger(int $min = 1, int $max = PHP_INT_MAX): int
+    protected static function positiveNonZeroRandomInteger(int $min = 1, int $max = PHP_INT_MAX): int
     {
-        if ($min < 1) $min = 1;
-        return $this->positiveRandomInteger($min, $max);
+        return new PositiveIntegerExceptZero(self::$faker, new IntRange($min, $max))->generate();
     }
 
     /**
      * Return a negative random integer except for zero.
      */
-    protected function negativeNonZeroRandomInteger(int $min = PHP_INT_MIN + 1, int $max = -1): int
+    protected static function negativeNonZeroRandomInteger(int $min = PHP_INT_MIN + 1, int $max = -1): int
     {
-        return $this->negativeRandomInteger($min, $max);
+        return self::negativeRandomInteger($min, $max);
     }
 
     /**
      * Return a random integer except for zero.
      */
-    protected function nonZeroRandomInteger(int $min = PHP_INT_MIN + 1, int $max = PHP_INT_MAX): int
+    protected static function nonZeroRandomInteger(int $min = PHP_INT_MIN + 1, int $max = PHP_INT_MAX): int
     {
-        do {
-            $number = $this->randomInteger($min, $max);
-        } while ($this->isZero($number));
-        return $number;
+        return new RelativeIntegerExceptZero(self::$faker, new IntRange($min, $max))->generate();
     }
     
     /**
      * Return a random relative float.
      */
-    protected function randomFloat(
-        float $min = -PHP_FLOAT_MAX, 
-        float $max = PHP_FLOAT_MAX, 
+    protected static function randomFloat(
+        float $min = FloatRange::MIN, 
+        float $max = FloatRange::MAX, 
         int $precision = PHP_FLOAT_DIG
     ): float {
-        if ($this->areBothNegative($min, $max))
-            return $this->negativeRandomFloat($min, $max, $precision);
-        if ($this->areBothPositive($min, $max))
-            return $this->positiveRandomFloat($min, $max, $precision);
-        if ($this->faker->boolean)
-            return $this->positiveRandomFloat(0, $max, $precision);
-        else
-            return $this->negativeRandomFloat($min, NextAfter::nextDown(0), $precision);
+        return new RelativeFloat(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a positive random float.
      */
-    protected function positiveRandomFloat(float $min = 0, float $max = PHP_FLOAT_MAX, int $precision = PHP_FLOAT_DIG): float
-    {
-        $precision = $this->normalizePrecision($precision);
-        if ($this->isNegative($min)) $min = 0;
-        if ($this->isNegative($max)) $max = 1;
-        return $this->faker->randomFloat($precision, $min, $max);
+    protected static function positiveRandomFloat(
+        float $min = 0, 
+        float $max = FloatRange::MAX, 
+        int $precision = PHP_FLOAT_DIG
+    ): float {
+        return new PositiveFloat(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a negative random float.
      */
-    protected function negativeRandomFloat(float $min = -PHP_FLOAT_MAX, float $max = 0, int $precision = PHP_FLOAT_DIG): float
-    {
-        $precision = $this->normalizePrecision($precision);
-        if ($this->isPositive($min)) $min = -1;
-        if ($this->isPositive($max)) $max = NextAfter::nextDown(0);
-        return -$this->faker->randomFloat($precision, abs($max), abs($min));
+    protected static function negativeRandomFloat(
+        float $min = FloatRange::MIN, 
+        float $max = 0, 
+        int $precision = PHP_FLOAT_DIG
+    ): float {
+        return new NegativeFloat(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a random relative float that is not a `float` type integer.
      */
-    protected function randomFloatStrict(float $min = -self::STRICT_FLOAT_MAX, float $max = self::STRICT_FLOAT_MAX, $precision = PHP_FLOAT_DIG): float
-    {
-        if ($this->areBothNegative($min, $max))
-            return $this->negativeRandomFloatStrict($min, $max, $precision);
-        if ($this->areBothPositive($min, $max))
-            return $this->positiveRandomFloatStrict($min, $max, $precision);
-        if ($this->faker->boolean)
-            return $this->positiveRandomFloatStrict(0, $max, $precision);
-        else
-            return $this->negativeRandomFloatStrict($min, precision: $precision);
+    protected static function randomFraction(
+        float $min = -FloatRange::MAX_FRACTION, 
+        float $max = FloatRange::MAX_FRACTION, 
+        $precision = PHP_FLOAT_DIG
+    ): float {
+        return new RelativeFraction(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a positive random float that is not an integer.
      */
-    protected function positiveRandomFloatStrict(float $min = 0, float $max = self::STRICT_FLOAT_MAX, $precision = PHP_FLOAT_DIG): float
-    {
-        $max = $this->limitPositiveStrictFloat($max);
-        do {
-            $number = $this->positiveRandomFloat($min, $max, $precision);
-        } while ($number == round($number, 0, RoundingMode::HalfTowardsZero));
-        return $number;
+    protected static function positiveRandomFraction(
+        float $min = 0, 
+        float $max = FloatRange::MAX_FRACTION, 
+        $precision = PHP_FLOAT_DIG
+    ): float {
+        return new PositiveFraction(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a negative random float that is not an integer.
      */
-    protected function negativeRandomFloatStrict(float $min = -self::STRICT_FLOAT_MAX, float $max = 0, $precision = PHP_FLOAT_DIG): float
-    {
-        $min = $this->limitNegativeStrictFloat($min);
-        do {
-            $number = $this->negativeRandomFloat($min, $max, $precision);
-        } while ($number == round($number, 0, RoundingMode::HalfTowardsZero));
-        return $number;
+    protected static function negativeRandomFraction(
+        float $min = -FloatRange::MAX_FRACTION, 
+        float $max = 0, 
+        $precision = PHP_FLOAT_DIG
+    ): float {
+        return new NegativeFraction(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a positve non zero random float.
      */
-    protected function positiveNonZeroRandomFloat(float $min = 0, float $max = PHP_FLOAT_MAX, $precision = PHP_FLOAT_DIG): float
+    protected static function positiveNonZeroRandomFloat(
+        float $min = 0, 
+        float $max = FloatRange::MAX, 
+        $precision = PHP_FLOAT_DIG): float
     {
-        do {
-            $number = $this->positiveRandomFloat($min, $max, $precision);
-        } while ($number == 0);
-        return $number;
+        return new PositiveExceptZero(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a negative non zero random float.
      */
-    protected function negativeNonZeroRandomFloat(float $min = -PHP_FLOAT_MAX, float $max = 0, $precision = PHP_FLOAT_DIG): float
-    {
-        do {
-            $number = $this->negativeRandomFloat($min, $max, $precision);
-        } while ($number == 0);
-        return $number;
+    protected static function negativeNonZeroRandomFloat(
+        float $min = FloatRange::MIN, 
+        float $max = 0, 
+        $precision = PHP_FLOAT_DIG
+    ): float {
+        return new NegativeExceptZero(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 
     /**
      * Return a random relative float except for zero.
      */
-    protected function nonZeroRandomFloat(float $min = -PHP_FLOAT_MAX, float $max = PHP_FLOAT_MAX, $precision = PHP_FLOAT_DIG): float
-    {
-        do {
-            $number = $this->randomFloat($min, $max, $precision);
-        } while ($number == 0);
-        return $number;
-    }
-
-    /**
-     * Limit the `$precision` between `0` and `PHP_FLOAT_DIG`.
-     */
-    private function normalizePrecision(int $precision): int
-    {
-        $precision = abs($precision);
-        if ($precision > PHP_FLOAT_DIG) $precision = PHP_FLOAT_DIG;
-        return $precision;       
-    }
-
-    /**
-     * If `$value` is negative limit to `PHP_INT_MIN + 1`.
-     */
-    private function limitNegativeInteger(int $value): int
-    {
-        if ($value === PHP_INT_MIN) return PHP_INT_MIN + 1;
-        return $value;
-    }
-
-    /**
-     * Limit the `$value` to the minimum number that can still have a fractional
-     * part.
-     */
-    private function limitNegativeStrictFloat(float $value): float
-    {
-        if ($value < -self::STRICT_FLOAT_MAX) return self::STRICT_FLOAT_MAX;
-        return $value;
-    }
-
-    /**
-     * Limit the `$value` to the maximum number that can still have a fractional
-     * part.
-     */
-    private function limitPositiveStrictFloat(float $value): float
-    {
-        if ($value > self::STRICT_FLOAT_MAX) return self::STRICT_FLOAT_MAX;
-        return $value;
+    protected static function nonZeroRandomFloat(
+        float $min = FloatRange::MIN, 
+        float $max = FloatRange::MAX, 
+        $precision = PHP_FLOAT_DIG
+    ): float {
+        return new RelativeExceptZero(self::$faker, new FloatRange($min, $max))->generate($precision);
     }
 }
