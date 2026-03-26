6f468f6f5bd5e9152ff76246732e9974ac0e93fd
Marco Consiglio <mrccnsgl@gmail.com>
2026-03-26 11:27:06 +0000

    test: `Validation\Float\OnlyPositive`

--- src/Validation/Float/OnlyPositive.php
+++ src/Validation/Float/OnlyPositive.php
@@ -3,14 +3,12 @@ namespace MarcoConsiglio\FakerPhpNumberHelpers\Validation\Float;
 
 use MarcoConsiglio\FakerPhpNumberHelpers\FloatRange;
 
-class OnlyPositive extends Validator
+class OnlyPositive extends Positive
 {
     public function validate(float &$min, float &$max): void
     {
-        if ($this->notAllowedFloat($min)) $min = 0.0;
-        if ($this->isNegative($min)) $min = 0.0;
-        if ($this->notAllowedFloat($max)) $max = FloatRange::MAX;
-        if ($this->isNegative($max)) $max = FloatRange::MAX;
+        $this->avoidNotAllowedFloats($min, $max);
+        $this->avoidNegativeFloats($min, $max);
         $this->swap($min, $max);
     }
 }
---
+++ src/Validation/Float/Positive.php
@@ -0,0 +1,29 @@
+<?php
+namespace MarcoConsiglio\FakerPhpNumberHelpers\Validation\Float;
+
+use MarcoConsiglio\FakerPhpNumberHelpers\FloatRange;
+
+abstract class Positive extends Validator
+{
+    protected function avoidNotAllowedFloats(float &$min, float &$max): void
+    {
+        if ($this->notAllowedFloat($min)) $this->setStandardMin($min);
+        if ($this->notAllowedFloat($max)) $this->setStandardMax($max);
+    }
+
+    protected function avoidNegativeFloats(float &$min, float &$max): void
+    {
+        if ($this->isNegative($min)) $this->setStandardMin($min);
+        if ($this->isNegative($max)) $this->setStandardMax($max);
+    }
+
+    protected function setStandardMin(float &$min): void
+    {
+        $min = 0.0;
+    }
+
+    protected function setStandardMax(float &$max): void
+    {
+        $max = FloatRange::MAX;
+    }
+}
